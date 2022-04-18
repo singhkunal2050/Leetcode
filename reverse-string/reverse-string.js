14887e4e23731ec0431cd90f5c3bee2a14a98d8a
Kunal SIngh <49939683+singhkunal2050@users.noreply.github.com>
2022-04-18 21:17:11 +0530

    Time: 98 ms, Memory: 49.3 MB - LeetHub

---
+++ reverse-string/reverse-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let pointer1 = 0;
+    let pointer2 = s.length-1;
+
+    while(pointer1<pointer2){
+
+    let temp=s[pointer1];
+    s[pointer1]=s[pointer2];
+    s[pointer2]=temp;
+    
+    pointer1++;
+    pointer2--;
+}
+};
