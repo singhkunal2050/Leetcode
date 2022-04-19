bcab8f4b029b95c2a3adec3826767edf6687a256
Kunal SIngh <49939683+singhkunal2050@users.noreply.github.com>
2022-04-19 11:28:55 +0530

    Time: 225 ms (5.07%), Space: 52.1 MB (5.03%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+
+/// Approach => Linear Traverse all items => check for 0 => store 2 arrays for zero indexes and then traverse again and compare coords with dummy arrays
+
+
+var setZeroes = function(matrix) {
+    let rows = new Array(matrix.length).fill(-1)
+    let cols = new Array(matrix[0].length).fill(-1)
+    
+    for(let i =0;i<matrix.length ; i++){
+        for(let j=0 ; j<matrix[i].length ; j++){
+            if(matrix[i][j]==0){
+                rows[i] = 0
+                cols[j] = 0
+            }
+        }
+    }
+    
+    for(let i =0;i<matrix.length ; i++){
+        if(rows[i]==0){
+            matrix[i].fill(0);
+        }
+        for(let j=0 ; j<matrix[i].length ; j++){
+            if(cols[j]==0){
+                matrix[i][j]=0;
+            }
+        }
+    }
+    
+    
+    console.log(matrix)
+};
