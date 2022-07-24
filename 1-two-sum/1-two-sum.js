4aff46f1849e39bc73ebac500d8ac3ed62470839
Kunal SIngh <49939683+singhkunal2050@users.noreply.github.com>
2022-07-24 22:38:40 +0530

    Time: 75 ms (90.97%), Space: 43.2 MB (29.84%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,15 +4,15 @@
  * @return {number[]}
  */
 
-// target - current should be available in the array (if yes get both the index and push them avoid multiple entries and we are done :) 
+// target - current should be available in the prevMap (if yes get both the index :) 
 var twoSum = function(nums, target) {
-    let myMap = {}
+    let prevMap = {}
     for(let i = 0 ; i<nums.length ; i++){
         let diff = target-nums[i];
-        if(myMap.hasOwnProperty(diff))
-            return [ myMap[diff] , i]
+        if(prevMap.hasOwnProperty(diff))
+            return [ prevMap[diff] , i]
         else
-            myMap[nums[i]] = i
+            prevMap[nums[i]] = i
     }
     return [];
 };
