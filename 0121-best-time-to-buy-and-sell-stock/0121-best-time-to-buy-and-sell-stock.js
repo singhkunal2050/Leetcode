57ad8f2fbff6d704a4d9a59abe4130e5551b3778
Kunal SIngh <49939683+singhkunal2050@users.noreply.github.com>
2024-02-18 18:17:17 +0530

    Time: 78 ms (41.89%), Space: 59.2 MB (21.38%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -8,10 +8,9 @@ var maxProfit = function(prices) {
     
     while(r < prices.length){
         let profit = prices[r] - prices[l]; 
+        maxProfit = Math.max(profit, maxProfit);
 
-        if(profit > 0){
-            maxProfit = Math.max(profit, maxProfit);
-        } else {
+        if(profit < 0){
             l = r; // found a low point so move left to the low point
         }
         r++;
