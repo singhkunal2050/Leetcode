531c240055c5555af76745f5252f985a32eef51a
Kunal SIngh <49939683+singhkunal2050@users.noreply.github.com>
2024-02-18 16:27:51 +0530

    Time: 59 ms (47.78%), Space: 50.3 MB (9.22%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -3,17 +3,38 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(numbers, target) {
-  let results = {};
+
+// Hashmap Technique
+// var twoSum = function(numbers, target) {
+//   let results = {};
     
-    for(let i = 0 ; i < numbers.length; i++) {
-        let offsetIndex = i+1;
-        let complement = target - numbers[i];
-        if(results.hasOwnProperty(complement)){
-            return [results[complement], offsetIndex]
-        } else {
-            results[numbers[i]] = offsetIndex;
-        }
+//     for(let i = 0 ; i < numbers.length; i++) {
+//         let offsetIndex = i+1;
+//         let complement = target - numbers[i];
+//         if(results.hasOwnProperty(complement)){
+//             return [results[complement], offsetIndex]
+//         } else {
+//             results[numbers[i]] = offsetIndex;
+//         }
+//     }
+//     return [];
+// };
+
+// [2,7,11,15]
+// Since the array is sorted we can use 2 pointer here
+var twoSum = function(numbers, target) {
+    let [l,r] = [0, numbers.length - 1];
+    
+    while(l < r) {
+        if(numbers[l] + numbers[r] === target){
+            return [l+1, r+1];
+        } else if(numbers[l] + numbers[r] > target) {
+            r--;
+        } else if(numbers[l] + numbers[r] < target) {
+            l++;
+        }    
     }
+    
     return [];
+    
 };
