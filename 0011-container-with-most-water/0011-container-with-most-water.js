5f81c7cbb81df54e82d3a6e397b59ac20a9c07e1
Kunal SIngh <49939683+singhkunal2050@users.noreply.github.com>
2024-02-18 17:36:38 +0530

    Time: 47 ms (99.68%), Space: 56.6 MB (43.61%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+// BRUTE FORCE
+// var maxArea = function(height) {
+//     let maxArea = -1;
+    
+//     for(let i = 0 ; i < height.length ; i++) {
+//         for(let j = i + 1 ; j < height.length ; j++) {
+//             let currentContainerHeight = Math.min(height[i], height[j]);
+//             let currentContainerWidth = j - i;
+//             let currentArea = currentContainerHeight * currentContainerWidth;
+//             maxArea = Math.max(currentArea, maxArea);;
+//         }
+//     }
+//     return maxArea;
+// };
+
+// 2 Pointer
+var maxArea = function(height) {
+    let maxArea = -1;
+    let [l, r] = [0, height.length -1];
+    
+    while(l < r) {
+        let currentContainerHeight = Math.min(height[l], height[r]);
+        let currentContainerWidth = r - l;
+        let currentArea = currentContainerHeight * currentContainerWidth;
+        maxArea = Math.max(currentArea, maxArea);
+        
+        if(height[l] <= height[r]){
+            l++;
+        } else {
+            r--;
+        }
+        
+        
+    }
+    
+    return maxArea;
+};
