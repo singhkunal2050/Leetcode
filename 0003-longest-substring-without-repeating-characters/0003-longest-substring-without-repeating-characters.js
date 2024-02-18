f5e3705af53e2270249386fc48eb3330aca29287
Kunal SIngh <49939683+singhkunal2050@users.noreply.github.com>
2024-02-18 18:41:37 +0530

    Time: 79 ms (68.21%), Space: 53.8 MB (32.83%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let l = 0;
+    let set = new Set();
+    let max = 0;
+    
+    for(let r = 0 ; r < s.length ; r++) {
+        while(set.has(s[r])) {
+            set.delete(s[l])
+            l++;
+        }
+        set.add(s[r]);
+        max = Math.max(max, set.size);
+    }
+    
+    return max;
+};
