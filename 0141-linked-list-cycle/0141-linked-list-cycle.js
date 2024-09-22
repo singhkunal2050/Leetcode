df0f7329deb6abdb614ad32110ee7fc94f01bf75
Kunal SIngh <49939683+singhkunal2050@users.noreply.github.com>
2024-09-22 18:59:58 +0530

    Time: 67 ms (42.71%), Space: 52.7 MB (77.51%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,6 +11,7 @@
  * @return {boolean}
  */
 // Have 2 pointers, fast and slow, while fast reaches the end, move fast by 2 and slow by 1, if fast and slow are same at any iteration, the loop is detected(Q: How can we compare a non primitive like that)
+// A: Because both are compared by their memory address which is same and not value
 var hasCycle = function(head) {
     let slow = head;
     let fast = head;
