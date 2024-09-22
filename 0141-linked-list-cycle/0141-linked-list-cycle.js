518d5fef4190439cc29219a0542fdd1053fb5cfe
Kunal SIngh <49939683+singhkunal2050@users.noreply.github.com>
2024-09-22 18:58:36 +0530

    Time: 62 ms (66.80%), Space: 52.9 MB (64.02%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -10,6 +10,7 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+// Have 2 pointers, fast and slow, while fast reaches the end, move fast by 2 and slow by 1, if fast and slow are same at any iteration, the loop is detected(Q: How can we compare a non primitive like that)
 var hasCycle = function(head) {
     let slow = head;
     let fast = head;
