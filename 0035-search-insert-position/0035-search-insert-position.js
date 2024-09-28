59da5db369909d5226a08172cfadf5af8b7aa754
Kunal SIngh <49939683+singhkunal2050@users.noreply.github.com>
2024-09-29 00:05:51 +0530

    Time: 56 ms (33.22%), Space: 49.4 MB (21.37%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -6,16 +6,13 @@
 var searchInsert = function(nums, target) {
     const index =  nums.indexOf(target) ;
     
-    console.log({index});
     // Not Found
     if(index == -1){
-        console.log('not found');
         for(let i = 0 ; i < nums.length ; i++ ){
             if(nums[i] > target){
                 return i;
             }
         }
-        console.log('no eleme greater than target')
         return nums.length;
         
     } else {
